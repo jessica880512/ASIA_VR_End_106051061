2e025a64215137980d8ebf7c698b73e84d265a04
jessica880512 <55180625+jessica880512@users.noreply.github.com>
2021-01-12 07:18:51 +0800

    20210112

---
+++ VR_End_106051061/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking.Types;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("人物動畫")]
+    public Animator Ani;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("內臟"))
+        {
+            Ani.SetTrigger("fall觸發器");
+        }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("場景");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ VR_End_106051061/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;//介面
+using Valve.VR;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text TextScore;
+    [Header("分數")]
+    public int score;
+    [Header("投進的分數")]
+    public int scoreIn;
+
+    [Header("丟中音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "內臟"&&other.transform.position.y>2)
+        {
+            Addscore();
+        }
+
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 2;
+        }
+    }
+
+    //分數
+    private void Addscore()
+    {
+        score += scoreIn;
+        TextScore.text = "分數 :" + score;
+
+        aud.PlayOneShot(soundIn,Random.Range(1f,3f));
+    }
+}
